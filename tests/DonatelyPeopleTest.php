62822b13e27912ded441211e5f121535bf0824cc
Bryan Monzon <monzonb@gmail.com>
2017-12-08 22:38:21 -0800

    clean up last test

--- tests/DonatelyPeopleTest.php
+++ tests/DonatelyPeopleTest.php
@@ -6,11 +6,6 @@ class DonatelyPeopleTest extends PHPUnit_Framework_TestCase
 {
     public function testPeopleGet()
     {
-        $DONATELY_API_KEY = getenv('DONATELY_API_KEY');
-        if (!$DONATELY_API_KEY) {
-            $this->markTestSkipped('No API key in ENV');
-        }
-
         $stub = $this->getMockBuilder('Donately\DonatelyClient')->disableOriginalConstructor()->getMock();
         $stub->method('get')->willReturn('foo');
 
@@ -19,12 +14,7 @@ class DonatelyPeopleTest extends PHPUnit_Framework_TestCase
     }
 
     public function testPersonGet()
-    {
-        $DONATELY_API_KEY = getenv('DONATELY_API_KEY');
-        if (!$DONATELY_API_KEY) {
-            $this->markTestSkipped('No API key in ENV');
-        }
-        
+    {     
         $stub = $this->getMockBuilder('Donately\DonatelyClient')->disableOriginalConstructor()->getMock();
         $stub->method('get')->willReturn('foo');
 
